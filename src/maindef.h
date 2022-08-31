5cb60fa857c2f2b4ba06bf972c78c970b3932725
Deniz Bahadir <deniz@code.bahadir.email>
2022-08-31 13:31:59 +0200

    Bump version to 2.54.1
    Signed-off-by: Deniz Bahadir <deniz@code.bahadir.email>

--- src/maindef.h
+++ src/maindef.h
@@ -13,7 +13,7 @@
 #define MAINDEF_H
 
 // Program version
-#define OBJCONV_VERSION         2.54
+#define OBJCONV_VERSION         2.54.1
 
 
 // Integer type definitions with platform-independent sizes:
